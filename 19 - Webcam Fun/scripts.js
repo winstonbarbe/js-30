a4e1fab324d953367d38ee80df70460310b5a3d4
winstonbarbe <winston.barbe@gmail.com>
2021-03-30 18:56:55 -0400

    implmenets green screen funtionality

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,7 +25,8 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
-    pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+    ctx.globalAlpha = 0.5;
     ctx.putImageData(pixels, 0, 0);
   }, 500);
 }
@@ -57,6 +58,33 @@ function rgbSplit(pixels) {
     pixels.data[i + 1000] = pixels.data[i + 1];
     pixels.data[i - 50] = pixels.data[i + 2] ;
   }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
   return pixels;
 }
 
