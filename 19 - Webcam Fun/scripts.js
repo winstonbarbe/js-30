5508a93ae9d3a58f5cb447deb24cd5add8dd1284
winstonbarbe <winston.barbe@gmail.com>
2021-03-30 18:10:21 -0400

    finishes take photo funtionality

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,7 +24,21 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-  }, 16);
+  }, 500);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "fugly");
+  link.innerHTML = (`<img src="${data}" alt="Ew">`);
+  strip.insertBefore(link, strip.firstChild);
 }
 
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
