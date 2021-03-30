93bda05f5302d44f57c0f3b0c74ff42af2ba4b72
winstonbarbe <winston.barbe@gmail.com>
2021-03-30 18:42:44 -0400

    creates red effect

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,6 +24,9 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 500);
 }
 
@@ -39,6 +42,14 @@ function takePhoto() {
   strip.insertBefore(link, strip.firstChild);
 }
 
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] = pixels.data[i] + 100;
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+    pixels.data[i + 2] = pixels.data[i] * 0.5;
+  }
+  return pixels;
+}
 getVideo();
 
 video.addEventListener("canplay", paintToCanvas);
